dd321d29221bee0a32bf54382cd6f9359779a946
Kalana Karunarathna <kalanasrilankarockets@gmail.com>
2025-02-26 14:28:14 +0530

    Add sidebars

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Background from '../components/Background.tsx';
 import Header from '../components/Header.tsx';
+import Left_Sidebar from '../components/L_Sidebar.tsx';
+import Right_Sidebar from '../components/R_Sidebar.tsx';
 
 const Grammar_checker_pages:React.FC = () => {
 
@@ -9,7 +11,11 @@ const Grammar_checker_pages:React.FC = () => {
       <Background>
         <div className='relative w-full h-full flex flex-col'>
             <Header />
-            
+            <div>
+                <Left_Sidebar />
+
+                <Right_Sidebar />
+            </div>
         </div>
       </Background>
     </>
