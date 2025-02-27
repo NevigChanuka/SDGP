3ee89178e67eca80fc7c1b4b967175e26e364246
Imandi <imandikavithma2714@gmail.com>
2025-02-27 17:08:13 +0530

    Added Signup form with Logo

---
+++ frontend/src/Pages/SignUp.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Logo}  from "../components/Logo";
+
+const SignUp: React.FC = () => {
+  return (
+    <div className="w-[30%] h-[640px] mx-auto mt-12 bg-white p-10 rounded-2xl shadow-2xl border border-green-900">
+        <Logo/>
+      <h2 className="text-center text-lg font-medium mt-2">Create an account</h2>
+
+      
+    </div>
+  );
+};
+
+export default SignUp;
