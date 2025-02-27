77d7273fe243d4bf551f8a38b3e4608ddbffb676
Supuni <kotuwegodagurugesupuni@gmail.com>
2025-02-27 22:51:10 +0530

    Create form structure and added logo

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import Background from '../components/Background.tsx';
-import Header from '../components/Header.tsx';
-import Left_Sidebar from '../components/L_Sidebar.tsx';
-import Right_Sidebar from '../components/R_Sidebar.tsx';
+
+import SignIn from '../Pages/SignIn.tsx';
 
 const Grammar_checker_pages:React.FC = () => {
 
   return (
     <>
-      <Background>
-        <div className='relative w-full h-full flex flex-col'>
-            <Header />
-            <div>
-                <Left_Sidebar />
+        <SignIn />
 
-                <Right_Sidebar />
-            </div>
-        </div>
-      </Background>
+        
     </>
   )
 }
 
 export default Grammar_checker_pages;
+
+
+
---
+++ frontend/src/Pages/SignIn.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Logo} from "../components/Logo";
+
+const SignIn: React.FC = () => {
+  return (
+    <div className="w-[30%] h-[640px] mx-auto mt-12 bg-white p-10 rounded-2xl shadow-2xl border border-green-900">
+      
+        <Logo/>
+      <h2 className="text-center text-lg font-medium mt-2">Sign In</h2>
+
+      
+    </div>
+  );
+};
+
+
+export default SignIn;
