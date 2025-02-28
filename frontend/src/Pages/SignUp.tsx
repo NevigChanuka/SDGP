ee240e6c3712022a74050c36053dececeed1039e
Imandi <imandikavithma2714@gmail.com>
2025-02-28 12:01:05 +0530

    Added social meadia button to signup

--- frontend/src/Pages/SignUp.tsx
+++ frontend/src/Pages/SignUp.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import {Logo}  from "../components/Logo";
+import GoogleIcon from "../assets/Google.png";
+import FacebookIcon from "../assets/Facebook.png";
+import AppleIcon from "../assets/Apple.png";
 
 const SignUp: React.FC = () => {
   return (
@@ -16,7 +19,7 @@ const SignUp: React.FC = () => {
                     id="name"
                     placeholder="Name"
                     autoComplete="off"
-                    className="mt-4 rounded border border-gray-300 h-10 px-3 w-full"
+                    className="mt-4 rounded border border-gray-300 h-8 px-3 w-full"
                 />
 
                 <input
@@ -24,7 +27,7 @@ const SignUp: React.FC = () => {
                     id="email"
                     placeholder="Email"
                     autoComplete="off"
-                    className="mt-4 rounded border border-gray-300 h-10 px-3 w-full"
+                    className="mt-4 rounded border border-gray-300 h-8 px-3 w-full"
                 />
 
                 <input
@@ -32,7 +35,7 @@ const SignUp: React.FC = () => {
                     id="password"
                     placeholder="Password"
                     autoComplete="off"
-                    className="mt-4 rounded border border-gray-300 h-10 px-3 w-full"
+                    className="mt-4 rounded border border-gray-300 h-8 px-3 w-full"
                 />
 
                 <input
@@ -40,10 +43,10 @@ const SignUp: React.FC = () => {
                     id="Confirm password"
                     placeholder="Confirm password"
                     autoComplete="off"
-                    className="mt-4 rounded border border-gray-300 h-10 px-3 w-full"
+                    className="mt-4 rounded border border-gray-300 h-8 px-3 w-full"
                 />
 
-                <button type="submit" className="mt-4 px-6 py-2 bg-green-500 text-white rounded-md hover:bg-green-600">
+                <button type="submit" className="mt-4 px-6 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 cursor-pointer">
                     Create
                 </button>
 
@@ -52,9 +55,24 @@ const SignUp: React.FC = () => {
         </form>
 
         <div className="flex items-center justify-center w-full">
-            <hr className="flex-grow h-[2px] bg-black border-none mx-2 sm:mx-4" />
+            <hr className="flex-grow h-[2px] bg-black border-none mx-2 sm:mx-4"/>
             <span className="mx-2 text-sm font-bold sm:text-base">OR</span>
-            <hr className="flex-grow h-[2px] bg-black border-none mx-2 sm:mx-4" />
+            <hr className="flex-grow h-[2px] bg-black border-none mx-2 sm:mx-4"/>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-full">
+            <button className="flex items-center justify-center w-4/5 h-8 px-4 my-1 text-sm font-bold text-black bg-white border border-black rounded-lg cursor-pointer">
+                <img src={GoogleIcon} alt="Google" className="w-4 h-4 mr-5"/>
+                Continue with Google
+            </button>
+            <button className="flex items-center justify-center w-4/5 h-8 px-4 my-1 text-sm font-bold text-black bg-white border border-black rounded-lg cursor-pointer">
+                <img src={FacebookIcon} alt="Facebook" className="w-5 h-5 mr-2.5"/>
+                Continue with Facebook
+            </button>
+            <button className="flex items-center justify-center w-4/5 h-8 px-4 my-1 text-sm font-bold text-black bg-white border border-black rounded-lg cursor-pointer">
+                <img src={AppleIcon} alt="Apple" className="w-3.5 h-3.5 mr-7"/>
+                Continue with Apple
+            </button>
         </div>
 
       
