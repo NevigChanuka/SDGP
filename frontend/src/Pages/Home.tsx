51ca361ee8950dfa6a394bc262874fe9c3fb3883
Supuni <kotuwegodagurugesupuni@gmail.com>
2025-03-04 17:28:39 +0530

    Updated the navigation bar.

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -10,6 +10,8 @@ const Home:React.FC = () => {
       <NavBar/>
 
       
+
+      
   )
 }
 
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -8,63 +8,40 @@ interface MenuLink {
 }
 
 const MenuLinks: MenuLink[] = [
-  {
-    id: 1,
-    name: "Home",
-    link: "/#home",
-  },
-  {
-    id: 2,
-    name: "FAQ",
-    link: "/#faq",
-  },
-  {
-    id: 3,
-    name: "FeedBack",
-    link: "/#feedback",
-  },
-
-  {
-    id: 4,
-    name: "Settings",
-    link: "/#settings",
-  },
- 
+  { id: 1, name: "Home", link: "/#home" },
+  { id: 2, name: "FAQ", link: "/#faq" },
+  { id: 3, name: "FeedBack", link: "/#feedback" },
+  { id: 4, name: "Settings", link: "/#settings" },
 ];
 
 const NavBar: React.FC = () => {
   return (
-    <nav>
-      <div className="container py-3 md:py-2">
-        <div className="flex justify-between items-center">
-          {/* Logo Section */}
-          <div>
-            <a href="#" className="flex items-center gap-3">
-              <img src={Logo} alt="Logo" className="w-45" />
-              
-            </a>
-          </div>
-
-          {/* NavLinks Section */}
-          <div>
-            <ul className="flex items-center gap-8">
-              {MenuLinks.map(({ id, name, link }) => (
-                <li key={id} className="cursor-pointer py-4">
-                  <a
-                    href={link}
-                    className="text-lg font-medium 
-                               hover:text-primary py-2 
-                               hover:border-b-2 
-                               hover:border-primary
-                               transition-all duration-300"
-                  >
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <nav >
+      <div className="container mx-auto py-3 px-6 flex justify-between items-center">
+        {/* Logo Section (Left Side) */}
+        <div>
+          <a href="#" className="flex items-center gap-3">
+            <img src={Logo} alt="Logo" className="w-30" />
+          </a>
         </div>
+
+        {/* Navigation Links (Center) */}
+        <div>
+          <ul className="flex items-center gap-8">
+            {MenuLinks.map(({ id, name, link }) => (
+              <li key={id} className="cursor-pointer py-4">
+                <a
+                  href={link}
+                  className="text-lg font-medium text-black-800 dark:text-black-200 hover:text-green-500 py-2 hover:border-b-2 hover:border-green-500 transition-all duration-300"
+                >
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+       
       </div>
     </nav>
   );
