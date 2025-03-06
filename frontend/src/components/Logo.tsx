da2b5ccd4ff664538c268d554e475847f02efdce
Imandi <imandikavithma2714@gmail.com>
2025-03-06 12:41:11 +0530

    Updated logo size on login pages

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png";
 export const Logo:React.FC = () => {
     return (
         <div className="flex items-center justify-center">
-            <img src={logo} alt="Logo" className="w-[60px] h-[60px]"/>
+            <img src={logo} alt="Logo" className="w-[130px] h-[60px]"/>
         </div>
     )
 }
