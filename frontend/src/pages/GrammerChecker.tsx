1b9cfea9577c730e229ae87d42a5ba54c34b07c0
Nevig Chanuka <nevig.chanuka@gmail.com>
2025-03-17 13:35:07 +0530

    redesign website

---
+++ frontend/src/pages/GrammerChecker.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const GrammerChecker: React.FC = () => {
+
+    return(
+
+        <div>
+
+            
+            
+        </div>
+    );
+
+
+
+}
+
+
+ 
+
+export default GrammerChecker;
