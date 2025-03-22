f4c5303b305b7d529bbee318ea55e25cd24e2b39
Kalana Karunarathna <kalanasrilankarockets@gmail.com>
2025-03-22 15:16:07 +0530

    Redesign Left sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
+import GrammarCheckerPage from "./pages/GrammerChecker.tsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/grammar-checker" element={<GrammarCheckerPage />} />
         </Routes>
       </Router>     
     </div>
--- frontend/src/pages/GrammerChecker.tsx
+++ frontend/src/pages/GrammerChecker.tsx
@@ -1,22 +1,18 @@
-import React from "react";
+import React from 'react';
+import Header from '../components/Header.tsx';
+import Right_Sidebar from '../components/R_Sidebar.tsx';
 
 
-const GrammerChecker: React.FC = () => {
-
-    return(
-
-        <div>
-
-            
-            
+const GrammarCheckerPage: React.FC = () => {
+    
+    return (
+        <div className='relative w-full flex flex-col'>
+            <Header />
+            <div> 
+                <Right_Sidebar/>
+            </div>
         </div>
-    );
-
-
-
+    )
 }
 
-
- 
-
-export default GrammerChecker;
+export default GrammarCheckerPage;
