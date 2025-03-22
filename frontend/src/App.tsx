8856499a64e8fa2eee754eda9beba0b036c1edbb
Kalana Karunarathna <kalanasrilankarockets@gmail.com>
2025-03-22 18:37:18 +0530

    Start feedback page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import GrammarCheckerPage from "./pages/GrammerChecker.tsx";
+import Feedback from "./pages/Feedback.tsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/grammar-checker" element={<GrammarCheckerPage />} />
+          <Route path="/feedback" element={<Feedback />} />
         </Routes>
       </Router>     
     </div>
