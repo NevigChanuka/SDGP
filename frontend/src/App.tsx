4426266906ac19198f32a624618fee38abe575cd
Kalana Karunarathna <kalanasrilankarockets@gmail.com>
2025-03-24 09:27:52 +0530

    Do some changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,7 @@ import Feedback from "./pages/Feedback.tsx";
 import SimilarWords from "./pages/SimilarPages.tsx";
 import WordsSet from "./pages/WordsSet.tsx";
 import Rules from "./pages/RulesPage.tsx";
-import SignIn from "./pages/SignIn.tsx";
-import SignUp from "./pages/SignUp.tsx";
+
 
 function App() {
   return (
@@ -20,8 +19,6 @@ function App() {
           <Route path="/similar-words" element={<SimilarWords />} />
           <Route path="/words-set" element={<WordsSet />} />
           <Route path="/rules" element={<Rules />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>     
     </div>
